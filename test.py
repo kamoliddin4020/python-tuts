980405416f1a26acd7fba7469cd3122c9cbe76ee
kamoliddin4020 <73027461+kamoliddin4020@users.noreply.github.com>
2020-10-25 16:47:09 +0500

    Added test.py

---
+++ test.py
@@ -0,0 +1,52 @@
+print('Hello, world!')
+print(10 * 'kamaz')
+print('sanak' + 'sanak')
+print(r'c:\docs\navin')
+print('Kamoliddin')
+print('K\namoliddin')
+
+x = 10
+y = 5
+
+print(x+y)
+x = 8**2
+y = 6
+print(x+y)
+print(x)
+print(x+y)
+name = 'e onayni'
+print (name)
+name = 'Bilol qachon endi dastur yaratamiz'
+print(name)
+name = 'Kamoliddin'
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+print(name[9])
+name = "mendirman o'sha"
+print(name + " e o'shani kallasiga qo'ydim")
+name= 'Roddom'
+print('Det'+name[3:])
+a = 'kalla'
+nums = '45,02,12,91'
+print(nums)
+print(nums[2:4])
+
+
+
+
+
+
+
+
+
+
+
+
+
